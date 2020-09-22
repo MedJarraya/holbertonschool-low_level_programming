37e5f616a644b922f73203104b5367ee9fece2b4
Mohamed Jarraya <blambertn@gmail.com>
2020-09-22 14:45:02 -0700

    3rd task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ **/
+
+void print_alphabet_x10(void)
+{
+int i = 0;
+char alpha;
+
+while (i < 10)
+{
+alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar (alpha);
+alpha++;
+}
+_putchar('\n');
+i++;
+}
+}
