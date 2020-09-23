b80d3cc7c15cd07e9e4ca847d1387372ee582fbf
Mohamed Jarraya <blambertn@gmail.com>
2020-09-23 14:19:32 -0700

    add

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints from n to 98
+ * @n: int
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+int a;
+
+	if (n < 98)
+	{
+		a = n;
+		while (a <= 98)
+		{
+			if (a != 98)
+			printf("%i, ", a);
+			else
+			printf("%i\n", a);
+			a++;
+		}
+	}
+	else
+	{
+		a = n;
+		while (a >= 98)
+		{
+			if (a != 98)
+			printf("%i, ", a);
+			else
+			printf("%i\n", a);
+			a--;
+		}
+	}
+}
