aacae2479e065125d92a9633dfd47409621b757d
Mohamed Jarraya <blambertn@gmail.com>
2020-09-29 05:55:56 -0700

    3rd task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: char
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (*s != '\0')
+{
+s++;
+i++;
+}
+return (i);
+}
