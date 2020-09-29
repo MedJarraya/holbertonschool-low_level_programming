6417aaecd55b6466b4d52d06cac768120afa7b4a
Mohamed Jarraya <blambertn@gmail.com>
2020-09-29 07:27:33 -0700

    4th task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts - puts
+ * @str: string
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar (*str);
+str++;
+}
+_putchar ('\n');
+}
