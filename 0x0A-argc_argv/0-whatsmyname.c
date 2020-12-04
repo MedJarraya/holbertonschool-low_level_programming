f3e53ade90fdec68e23a5667448883a78e96b72c
Mohamed Jarraya <blambertn@gmail.com>
2020-12-04 07:37:15 -0800

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - display 
+ * @argc: variable
+ * @argv: string
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int p;
+
+	for (p = 0; p < argc; p++)
+	printf("%s\n", argv[p]);
+	return (0);
+}
