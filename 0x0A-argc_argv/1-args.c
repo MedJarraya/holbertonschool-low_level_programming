f6a202887ff79a610380de9ebf017e18c7167b57
Mohamed Jarraya <blambertn@gmail.com>
2020-12-04 07:59:34 -0800

    2nd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - display
+ * @argc: variable
+ * @argv: variable
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
