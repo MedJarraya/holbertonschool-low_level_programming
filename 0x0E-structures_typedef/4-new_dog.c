613f10334a992e3dd1882b200e28388eb9dcf4cf
Mohamed Jarraya <blambertn@gmail.com>
2020-12-06 07:00:04 -0800

    lastfix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * new_dog - new doggo house
- * @name: new doggo name
- * @age: new doggo age
- * @owner: new doggo ownername
- * Return: new_doggo
+ * new_dog - dog in the house
+ * @name: new name for the dog
+ * @age: new age for the dog
+ * @owner: new owner for the dog
+ * Return: new_dog
  */
-dog_t *new_doggo(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_doggo;
+	struct dog *new_dog;
 
-	new_doggo = malloc(sizeof(struct dog));
-	if (new_doggo == NULL)
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
 		return (NULL);
-	new_doggo->name = name;
-	new_doggo->age = age;
-	new_doggo->owner = owner;
-	return (new_doggo);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
 }
