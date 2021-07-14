521a83647bec14d74a3f7906d119a06b2c1aaf4d
Mohamed Jarraya <blambertn@gmail.com>
2021-07-14 09:38:06 -0700

    C

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python3
-"""
-Module function
-"""
+"""module"""
+
 
 def island_perimeter(grid):
-    """ Returns the perimeter of the island described in grid."""
-
-    perim = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-
-            if grid[i][j] == 1:
-
-                try:
-                    if grid[i - 1][j] == 0 or i == 0:
-                        perim += 1
-                except IndexError:
-                    perim += 1
-
-                try:
-                    if grid[i + 1][j] == 0:
-                        perim += 1
-                except IndexError:
-                    perim += 1
-
-                try:
-                    if grid[i][j - 1] == 0 or j == 0:
-                        perim += 1
-                except IndexError:
-                    perim += 1
-
-                try:
-                    if grid[i][j + 1] == 0:
-                        perim += 1
-                except IndexError:
-                    perim += 1
-    return perim
+    """returns the perimeter of an island"""
+    x = len(grid)
+    y = len(grid[0])
+    perimeter = 0
+    for i in range(x):
+        for j in range(y):
+            if grid[i][j] == 0:
+                continue
+            if i == 0 or grid[i - 1][j] == 0:
+                perimeter += 1
+            if i == x - 1 or grid[i + 1][j] == 0:
+                perimeter += 1
+            if j == 0 or grid[i][j - 1] == 0:
+                perimeter += 1
+            if j == y - 1 or grid[i][j + 1] == 0:
+                perimeter += 1
+    return perimeter
