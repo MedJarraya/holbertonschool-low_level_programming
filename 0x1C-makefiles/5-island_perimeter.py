c02d4bb66c55a4d8dd6ced7d2f1bac595afc32e4
Mohamed Jarraya <blambertn@gmail.com>
2021-07-14 09:36:13 -0700

    C

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module function
+"""
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid."""
+
+    perim = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+
+            if grid[i][j] == 1:
+
+                try:
+                    if grid[i - 1][j] == 0 or i == 0:
+                        perim += 1
+                except IndexError:
+                    perim += 1
+
+                try:
+                    if grid[i + 1][j] == 0:
+                        perim += 1
+                except IndexError:
+                    perim += 1
+
+                try:
+                    if grid[i][j - 1] == 0 or j == 0:
+                        perim += 1
+                except IndexError:
+                    perim += 1
+
+                try:
+                    if grid[i][j + 1] == 0:
+                        perim += 1
+                except IndexError:
+                    perim += 1
+    return perim
