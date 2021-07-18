d24e5f877e8a998f6a5bc4103aa1be08a1931905
Ubuntu <blambertn@gmail.com>
2021-07-18 14:15:16 -0700

    C

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *aux;
+	unsigned int state = 0;
+	unsigned int index = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (index < ht->size)
+	{
+		aux = ht->array[index];
+		while (aux)
+		{
+			if (state == 0)
+				state = 1;
+			else
+				printf(", ");
+
+			printf("'%s': '%s'", aux->key, aux->value);
+			aux = aux->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
